b69ba10072091ae6ddf3a97dc4b66ea2e79c08d3
Tan Choon Yong <95896179+TanCY1@users.noreply.github.com>
2023-03-28 01:23:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function getscreensize() {
     document.getElementById("screenwidth").innerHTML = window.innerWidth;
 }
+document.getElementById("screenwidth").innerHTML = window.innerWidth;
 window.addEventListener('resize', getscreensize);
