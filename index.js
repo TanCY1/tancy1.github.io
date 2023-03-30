53fa3ab980f5a624452739ae127786a9364660a3
Tan Choon Yong <95896179+TanCY1@users.noreply.github.com>
2023-03-30 04:13:34 +0000

    Update

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const goldenRatio=(Math.sqrt(5)+1)/2
-jQuery("#profile").fitText(1.618);
+jQuery("#profile").fitText((Math.sqrt(5) + 1) / 2);
+function format() {
+  if (window.innerHeight / window.innerWidth >= 1) {
+    document.getElementById("backg").style.flexDirection = "column";
+    document.getElementById("img").style.maxWidth = "50vw";
+    document.getElementbyId("profile").style.height="75vh";
+  }
+  else {
+    document.getElementById("backg").style.flexDirection = "row";
+    document.getElementById("img").style.maxWidth = "25vw";
+    document.getElementbyId("profile").style.height="0";
+  }
+}
+window.addEventListener("resize", format);
+
